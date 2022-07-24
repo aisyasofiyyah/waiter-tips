749c38d582d28251f51392f8048b9915b612d6eb
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-24 23:25:11 +0800

    Update tips.py

--- tips.py
+++ tips.py
@@ -8,15 +8,17 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>Waiter Tips Prediction App</h1>", unsafe_allow_html=True)
-st.subheader("Waiter Tips App is where we analyse the tips given to a waiter for serving the food in a restaurant depends on what kind of factors and predict the tips.")
+st.text("Waiter Tips App is where we analyse the tips given to a waiter for serving the food in a restaurant depends on what kind of factors and predict the tips.")
 
 tips = pd.read_csv('https://raw.githubusercontent.com/aisyasofiyyah/waiter-tips/main/tips.csv')
 
-st.sidebar.markdown("Reference: \nZahra Amini, [Kaggle](https://www.kaggle.com/datasets/aminizahra/tips-dataset)")
+st.sidebar.markdown("""Reference: 
+                    \nZahra Amini, [Waiter's Tips Dataset](https://www.kaggle.com/datasets/aminizahra/tips-dataset)
+                    \nAman Kharwal, [Waiter Tips Prediction with Machine Learning](https://thecleverprogrammer.com/2022/02/01/waiter-tips-prediction-with-machine-learning/)
+                    """)
 tab1, tab2, tab3 = st.tabs(["Analysis 1", "Analysis 2", "Analysis 3"])
 
 with tab1:
-          st.header("Analysis 1")
           st.markdown("""Tips given to the waiters according to:
           \nthe total bill paid, number of people at a table and the day of the week""")
 
@@ -24,7 +26,6 @@ with tab1:
           st.write(figure)
 
 with tab2:
-          st.header("Analysis 2")
           st.markdown("""Tips given to the waiters according to: 
                   \nthe total bill paid, the number of people at a table and the gender of the person paying the bill""")
 
@@ -32,7 +33,6 @@ with tab2:
           st.write(figure)
 
 with tab3:
-          st.header("Analysis 3")
           st.markdown("""Tips given to the waiters according to the days to find out which day the most tips are given to the waiters""")
 
           figure = px.pie(tips, values='tip', names='day',hole = 0.5)
