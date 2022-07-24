a8965bec0c9d0511acd40d531b0b86ec32f6e1e2
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-24 23:22:52 +0800

    Update tips.py

--- tips.py
+++ tips.py
@@ -8,27 +8,32 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>Waiter Tips Prediction App</h1>", unsafe_allow_html=True)
+st.subheader("Waiter Tips App is where we analyse the tips given to a waiter for serving the food in a restaurant depends on what kind of factors and predict the tips.")
 
 tips = pd.read_csv('https://raw.githubusercontent.com/aisyasofiyyah/waiter-tips/main/tips.csv')
 
-st.markdown("""Tips given to the waiters according to:
-          \nthe total bill paid
-          \nnumber of people at a table
-          \nand the day of the week""")
+st.sidebar.markdown("Reference: \nZahra Amini, [Kaggle](https://www.kaggle.com/datasets/aminizahra/tips-dataset)")
+tab1, tab2, tab3 = st.tabs(["Analysis 1", "Analysis 2", "Analysis 3"])
 
-figure = px.scatter(data_frame = tips, x="total_bill",y="tip", size="size", color= "day",trendline="ols")
-st.write(figure)
+with tab1:
+          st.header("Analysis 1")
+          st.markdown("""Tips given to the waiters according to:
+          \nthe total bill paid, number of people at a table and the day of the week""")
 
-st.markdown("""Tips given to the waiters according to: 
-        \nthe total bill paid, the number of people at a table and the gender of the person paying the bill""")
+          figure = px.scatter(data_frame = tips, x="total_bill",y="tip", size="size", color= "day",trendline="ols")
+          st.write(figure)
 
-figure = px.scatter(data_frame = tips, x="total_bill",
-                    y="tip", size="size", color= "sex", trendline="ols")
-st.write(figure)
+with tab2:
+          st.header("Analysis 2")
+          st.markdown("""Tips given to the waiters according to: 
+                  \nthe total bill paid, the number of people at a table and the gender of the person paying the bill""")
 
-st.markdown("""Tips given to the waiters according to the days to find out which day the most tips are given to the waiters""")
+          figure = px.scatter(data_frame = tips, x="total_bill", y="tip", size="size", color= "sex", trendline="ols")
+          st.write(figure)
 
-figure = px.pie(tips, 
-             values='tip', 
-             names='day',hole = 0.5)
-st.write(figure)
+with tab3:
+          st.header("Analysis 3")
+          st.markdown("""Tips given to the waiters according to the days to find out which day the most tips are given to the waiters""")
+
+          figure = px.pie(tips, values='tip', names='day',hole = 0.5)
+          st.write(figure)
