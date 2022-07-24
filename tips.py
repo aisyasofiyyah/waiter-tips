627889ce2963cd499a17d6395feb2e443e640756
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-24 22:49:39 +0800

    Create tips.py

---
+++ tips.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+st.set_page_config(layout="wide")
+st.markdown("<h1 style='text-align: center;'>Waiter Tips Prediction App</h1>", unsafe_allow_html=True)
