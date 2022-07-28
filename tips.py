cb99fd85d2cbe2db7dc12074abe571503d6aa4e2
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-28 17:35:17 +0800

    Update tips.py

--- tips.py
+++ tips.py
@@ -72,8 +72,7 @@ else:
   plt.xlabel("total_bill")
   plt.ylabel("tips")
   plt.legend()
-  f.show()
-  st.plotly_chart(f)
+  st.write(f)
   
 st.sidebar.markdown("""Reference: 
                       \n[1)](https://www.kaggle.com/datasets/aminizahra/tips-dataset) Zahra Amini, Waiter's Tips Dataset
